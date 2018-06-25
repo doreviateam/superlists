0ca29b6b2569b598bbafe87ff608ab16629694c4
doreviateam <doreviateam@gmail.com>
2018-06-25 21:27:57 +0200

    Basic view now returns minimal HTML

--- lists/tests.py
+++ lists/tests.py
@@ -1,3 +1,5 @@
+# pylint: disable-all
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 
@@ -5,6 +7,15 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
+    
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        doc_html = response.content.decode('utf8')
+
+        self.assertTrue(doc_html.startswith('<html>'))
+        self.assertIn('<title>To-Do lists</title>', doc_html)
+        self.assertTrue(doc_html.endswith('</html>'))
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
