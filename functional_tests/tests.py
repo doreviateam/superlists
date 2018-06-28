8a0d13ab6cc2b433901a7e9944dba6ac458e417d
doreviateam <doreviateam@gmail.com>
2018-06-28 22:55:28 +0200

    Use Bootstrap to improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 import time
 import unittest
 
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -10,7 +10,7 @@ from selenium.common.exceptions import WebDriverException
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -129,7 +129,7 @@ class NewVisitorTest(LiveServerTestCase):
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=150
         )
 
         # She starts a new list and sees the input is nicely centered there too
@@ -141,5 +141,5 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
 
         self.assertAlmostEqual(
-            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=10
+            inputbox.location['x'] + inputbox.size['width'] / 2, 512, delta=150
         )
